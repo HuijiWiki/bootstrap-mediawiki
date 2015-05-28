c7344f052b472d58355f4f4e173261b9101a87ab
root <guxi99@gmail.com>
2015-05-28 17:07:52 +0800

    hide discussion panel in main page

--- js/behavior.js
+++ js/behavior.js
@@ -411,10 +411,12 @@ $(function() {
             var talkCount = data.flow["view-topiclist"].result.topiclist.roots.length;
             if (talkCount > 0){
                 $("#ca-talk a").append("<sup>&nbsp;<span class='badge'>"+talkCount+"</span></sup>");
-                flowAdapter.init(data);
-                var items = flowAdapter.convert(data);
-                var html = flowAdapter.adapt(items, {postLimit:2, topicLimit:2});
-                $('#mw-content-text').append(html);
+                if (!mw.config.get('wgIsMainPage')){
+                    flowAdapter.init(data);
+                    var items = flowAdapter.convert(data);
+                    var html = flowAdapter.adapt(items, {postLimit:2, topicLimit:2});
+                    $('#mw-content-text').append(html);
+                }
             }
         });
 
