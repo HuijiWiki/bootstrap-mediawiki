07e8fcd0fbeb029e368a856c3dc743d368eb8216
root <guxi99@gmail.com>
2015-08-06 15:04:23 +0800

    add new fiels

---
+++ wgSiteNotice.php
@@ -0,0 +1,7 @@
+<?php
+$wgSiteNotice = '<div class="bs-callout bs-callout-primary">
+<h4>您的登机牌</h4>
+灰机停机坪上已经停满了灰机，不知道您手中的登机牌是属于哪一架灰机的呢？
+[[huiji:灰机停机坪|<span class="mw-ui-button">灰机停机坪</span>]]
+</div>';
+?>
