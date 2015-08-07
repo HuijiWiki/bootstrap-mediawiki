404d66a54e0bf7e9f4d2b5c860a8b84bf3a33b98
root <guxi99@gmail.com>
2015-08-07 11:54:04 +0800

    new notice

--- wgSiteNotice.php
+++ wgSiteNotice.php
@@ -1,7 +1,55 @@
 <?php
-$wgSiteNotice = '<div class="bs-callout bs-callout-primary">
+$wgSiteNotice = '
+<choose>
+<option weight="2">
+<div class="bs-callout bs-callout-primary">
 <h4>您的登机牌</h4>
 灰机停机坪上已经停满了灰机，不知道您手中的登机牌是属于哪一架灰机的呢？
 [[huiji:灰机停机坪|<span class="mw-ui-button">灰机停机坪</span>]]
-</div>';
+</div>
+</option>
+<option>
+<div class="bs-callout bs-callout-success">
+<h4>7月月最佳</h4>
+荣誉属于[[User:AemonTargaryen]]，祝贺他！
+</div>
+</option>
+<option>
+<div class="bs-callout bs-callout-default">
+<h4>你知道吗？</h4>
+你可以在吐槽和留言板中使用‘@’功能，来提醒其他用户注意……
+[[witcher:首页|<span class="mw-ui-button">访问猎魔人维基</span>]]
+</div>
+</option>
+</option>
+<option>
+<div class="bs-callout bs-callout-default">
+<h4>你认识他们吗？</h4>
+TFBOYS是新生代偶像组合 由北京时代峰峻文化艺术发展有限公司于2013年8月6日推出。由王俊凯、王源和易烊千玺3名成员组成。
+[[tfboys:|<span class="mw-ui-button">访问TFBOYS维基</span>]]
+</div>
+</option>
+<option>
+<div class="bs-callout bs-callout-default">
+<h4>你可能喜欢的</h4>
+[[File:witcher-tuijian.jpg|left|link=witcher:]]猎魔人（Witcher)是那些专门被普通人雇佣来猎杀怪物的变种人类。他们在孩童时期，就开始接受炼金术的身体改造，最后他们将通过青草试炼来决定是否能够……
+[[witcher:首页|<span class="mw-ui-button">访问猎魔人维基</span>]]
+</div>
+</option>
+<option>
+<div class="bs-callout bs-callout-primary">
+<h4>关注我们</h4>
+本维基由灰机wiki提供技术支持。欢迎关注我们。<br>
+<i class="fa fa-weibo"></i> [http://weibo.com/u/5589792615 新浪微博@灰机wiki] <br>
+<i class="fa fa-envelope-o"></i> 客服邮箱：support@huiji.wiki<br>
+</div>
+</option>
+<option>
+<div class="bs-callout bs-callout-info">
+<h4>客官，请留步</h4>
+本维基的所有词条都是由像你一样的爱好者编写的……为什么不参与进来呢？<br>
+[{{canonicalurl:{{PAGENAME}}|action=edit}} <span class="mw-ui-button">编辑这个词条！</span>]
+</div>
+</option>
+</choose>';
 ?>
