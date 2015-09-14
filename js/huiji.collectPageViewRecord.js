1a350c55b554016323747c3a2bbf93cd0ba11197
root <guxi99@gmail.com>
2015-09-14 12:54:48 +0800

    add edit view records

--- js/huiji.collectPageViewRecord.js
+++ js/huiji.collectPageViewRecord.js
@@ -172,7 +172,6 @@ function getViewRecordsOnWikiSiteFromUserIdGroupByDay(userId,wikiSite,fromTime,t
 
 function getPreviousViewRecords(wikiSite,dayNumber,cb)
 {
-	var now = moment();
 	var date_array = new Array();
 	var number_array = new Array();
 	
@@ -208,10 +207,47 @@ function getPreviousViewRecords(wikiSite,dayNumber,cb)
 
 	getViewRecordsOnWikiSiteFromUserIdGroupByDay(-1,wikiSite,moment().subtract(1+dayNumber,"days").format("YYYY-MM-DD"),moment().subtract(1,"days").format("YYYY-MM-DD"),callback);
 
+}
+
+function getPreviousEditRecords(wikiSite,dayNumber,cb)
+{
+	var date_array = new Array();
+	var number_array = new Array();
+	
+	for(var i=1;i<=dayNumber;i++){
+		date_array[i-1] = moment().subtract(dayNumber-i+1,"days").format("YYYY-MM-DD");
+	}
+
+
+	var callback = function(data){
+		
+		var newData = {};
+		if(data.result != null){
+			var hashtable = new Array();
+			for(var i=0;i<data.result.length;i++){
+				hashtable[data.result[i]._id] = data.result[i].value; 
+			}
+			var rs = {};
+			rs.date_array = date_array;
+			for(var i=0;i<date_array.length;i++){
+				if(hashtable[date_array[i]]== null){
+					number_array[i] = 0;
+				}else{
+					number_array[i] = hashtable[date_array[i]];
+				}
+			}
+			rs.number_array = number_array;
+			newData.result = rs;
+			newData.status = data.status;
+		}
+		cb(newData);
+	}
+
+
+	getEditRecordsOnWikiSiteFromUserIdGroupByDay(-1,wikiSite,moment().subtract(1+dayNumber,"days").format("YYYY-MM-DD"),moment().subtract(1,"days").format("YYYY-MM-DD"),callback);
+
 
 
 }
-
-
 
 
