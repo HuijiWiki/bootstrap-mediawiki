36cbab8011c80d52cf50adeffbe8a6f295df9969
谷溪 <guxi99@gmail.com>
2015-10-15 19:26:56 +0800

    Update BootstrapMediaWiki.skin.php
    move contentSub into <small>

--- BootstrapMediaWiki.skin.php
+++ BootstrapMediaWiki.skin.php
@@ -185,22 +185,23 @@ class BootstrapMediaWikiTemplate extends HuijiSkinTemplate {
                                             </div>                                   
                                         <?php }
                                     } ?>
-                                <div id="contentSub">
-                                    <small><?php $this->html('subtitle') ?>
-                                <?php
-                                    if ($this->data['isarticle'] &&  !($this->getSkin()->getTitle()->isMainPage()) && $this->getSkin()->getTitle()->exists()){
-                                        $revPageId = $this->getSkin()->getTitle()->getArticleId();
-                                        $editinfo = UserStats::getLastEditer($revPageId,$wgHuijiPrefix);
-                                        $userPage = Title::makeTitle( NS_USER, $editinfo['rev_user_text'] );
-                                        $userPageURL = htmlspecialchars( $userPage->getFullURL() );
-                                        $bjtime = strtotime( $editinfo['rev_timestamp'] ) + 8*60*60;
-                                        $edittime = CommentFunctions::getTimeAgo( $bjtime );
-                                        echo '<a class="mw-ui-anchor mw-ui-progressive mw-ui-quiet" href="'.$userPageURL.'">'.$editinfo['rev_user_text'].'</a>&nbsp于'.$edittime.'前编辑了此页面';
-                                        echo '<div class="bdsharebuttonbox pull-right hidden-sm hidden-xs" data-tag="share_2"><a href="#" class="icon-weixin-share" data-tag="share_2" data-cmd="weixin" title="分享到微信"></a><a href="#" class="icon-weibo-share" data-tag="share_2" data-cmd="tsina" title="分享到新浪微博"></a><a href="#" class="icon-qqspace-share" data-tag="share_2" data-cmd="qzone" title="分享到QQ空间"></a><a href="#" class="icon-tieba-share" data-tag="share_2" data-cmd="tieba" title="分享到百度贴吧"></a><a href="#" class="icon-douban-share" data-tag="share_2" data-cmd="douban" title="分享到豆瓣网"></a></div>';
-                                    }
-                                ?>
-                                    </small>
-                                </div>
+                                <small>
+                                    <div id="contentSub">
+                                    <?php $this->html('subtitle') ?>
+                                    <?php
+                                        if ($this->data['isarticle'] &&  !($this->getSkin()->getTitle()->isMainPage()) && $this->getSkin()->getTitle()->exists()){
+                                            $revPageId = $this->getSkin()->getTitle()->getArticleId();
+                                            $editinfo = UserStats::getLastEditer($revPageId,$wgHuijiPrefix);
+                                            $userPage = Title::makeTitle( NS_USER, $editinfo['rev_user_text'] );
+                                            $userPageURL = htmlspecialchars( $userPage->getFullURL() );
+                                            $bjtime = strtotime( $editinfo['rev_timestamp'] ) + 8*60*60;
+                                            $edittime = CommentFunctions::getTimeAgo( $bjtime );
+                                            echo '<a class="mw-ui-anchor mw-ui-progressive mw-ui-quiet" href="'.$userPageURL.'">'.$editinfo['rev_user_text'].'</a>&nbsp于'.$edittime.'前编辑了此页面';
+                                            echo '<div class="bdsharebuttonbox pull-right hidden-sm hidden-xs" data-tag="share_2"><a href="#" class="icon-weixin-share" data-tag="share_2" data-cmd="weixin" title="分享到微信"></a><a href="#" class="icon-weibo-share" data-tag="share_2" data-cmd="tsina" title="分享到新浪微博"></a><a href="#" class="icon-qqspace-share" data-tag="share_2" data-cmd="qzone" title="分享到QQ空间"></a><a href="#" class="icon-tieba-share" data-tag="share_2" data-cmd="tieba" title="分享到百度贴吧"></a><a href="#" class="icon-douban-share" data-tag="share_2" data-cmd="douban" title="分享到豆瓣网"></a></div>';
+                                        }
+                                    ?>
+                                    </div>
+                                </small>
                             </h1>
                         </div>  
                         <?php if ( $this->data['isarticle'] ) { ?><div id="siteSub" class="alert alert-info visible-print-block" role="alert"><?php $this->msg( 'tagline' ); ?></div><?php } ?>
