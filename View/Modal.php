c4be3e07ddae4cd57967cfda19fcfc50cd6a430a
root <guxi99@gmail.com>
2015-11-27 14:09:14 +0800

    zs

--- View/Modal.php
+++ View/Modal.php
@@ -50,57 +50,34 @@
                     <div class="modal-content">
                         <div class="modal-header">
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                            <h4 class="modal-title" id="ModalLabel">登录</h4>
+                            <h5 class="modal-title" id="ModalLabel">登录</h5>
                         </div>
                         <div class="modal-body">
                             <div class="mw-ui-container login-wrap">
-                                <div class="userloginprompt"></div>
                                 <div class="userloginForm">
-                                    <form name="userlogin" class="mw-ui-vform" method="post">
-                                        <section class="mw-form-header">
-                                        </section>
-
-                                        <div class="mw-ui-vform-field user-name">
-                                            <label for="wpName1">
-                                                用户名
-                                            </label>
-                                            <input id="login-user-name" class="loginText mw-ui-input form-control" type="text"  tabindex="1" size="20" placeholder="请输入你的用户名" name="wpName">
-                                        </div>
-
+                                    <form name="userlogin" method="post">
                                         <div class="mw-ui-vform-field">
-                                            <label for="wpPassword1">
-                                                密码 <a href="/wiki/%E7%89%B9%E6%AE%8A:%E9%87%8D%E8%AE%BE%E5%AF%86%E7%A0%81" title="特殊:重设密码" class="mw-ui-flush-right">忘记密码？</a>
-                                            </label>
-                                            <input id="login-user-password" class="loginPassword mw-ui-input form-control"  tabindex="2" size="20" autofocus="" placeholder="请输入你的密码" type="password" name="wpPassword">
+                                            <input id="login-user-name" class="loginText" type="text" placeholder="请输入你的用户名">
+                                            <?php echo Linker::linkKnown( SpecialPage::getTitleFor('Userlogin'), '注册账户', array('id' => 'pt-createaccount' ),array('type' => 'signup') ); ?>
                                         </div>
-
-
-
                                         <div class="mw-ui-vform-field">
-                                            <div class="">
-                                                <input name="wpRemember" type="checkbox" value="1" id="wpRemember" tabindex="4" style="margin-right: 5px;"><label for="wpRemember">
-                                                    记住我的登录状态</label>
-                                            </div>
+                                            <input id="login-user-password" class="loginPassword"  placeholder="请输入你的密码" type="password" name="wpPassword">
+                                            <a href="/wiki/%E7%89%B9%E6%AE%8A:%E9%87%8D%E8%AE%BE%E5%AF%86%E7%A0%81" title="特殊:重设密码" class="mw-ui-flush-right">忘记密码？</a>
                                         </div>
-
                                         <div class="mw-ui-vform-field">
-                                            <input id="wpLoginAttempt" tabindex="6" class="mw-ui-button  mw-ui-block mw-ui-constructive" type="button" value="登录" name="wpLoginAttempt">
+                                                <input name="wpRemember" type="checkbox" value="1" id="wpRemember" tabindex="4" ><label for="wpRemember">记住我</label>
                                         </div>
-
-    <!--                                     <div class="mw-ui-vform-field" id="mw-userlogin-help">
-                                            <a href="https://www.mediawiki.org/wiki/Special:MyLanguage/Help:Logging_in">登录帮助</a>
-                                        </div> -->
-
-                                        <div id="mw-createaccount-cta">
-                                            没有账户？<?php echo Linker::linkKnown( SpecialPage::getTitleFor('Userlogin'), '注册', array('id' => 'pt-createaccount' ),array('type' => 'signup') ); ?>
+                                        <div class="mw-ui-vform-field">
+                                            <input id="wpLoginAttempt" tabindex="6" class="mw-ui-button  mw-ui-block mw-ui-constructive" type="button" value="登 录" name="wpLoginAttempt">
                                         </div>
-                                        <input type="hidden" name="wpLoginToken" value="5b59d95be44d1173971ec0b44d9fffa4">
+                                        <div class="mw-ui-vform-field api-login">
+                                            <span>联合登录</span>
+                                            <a class="icon-weibo-share"></a>
+                                            <a href="https://graph.qq.com/oauth2.0/authorize?response_type=code&client_id=101264508&redirect_uri=http://test.huiji.wiki/wiki/special:callbackqq" class="icon-qq-share"></a>
+                                        </div>
                                     </form>
                                 </div>
                             </div>
-
-
-
                         </div>
     <!--                    <div class="modal-footer">-->
     <!--                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>-->
