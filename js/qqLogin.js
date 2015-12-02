1c60bc07b2dcd1bab90287cdbff21c052ffbe7e9
root <guxi99@gmail.com>
2015-12-02 18:03:20 +0800

    del

--- js/qqLogin.js
+++ js/qqLogin.js
@@ -1,9 +1,5 @@
 ﻿$(document).ready(function(){
 
-    // check weibo login WB2.checkLogin()
-    if (WB2.checkLogin()==true && mw.config.get('wgUserId') == null) {
-        $('.qqlogin').modal({backdrop: 'static', keyboard: false});
-    };
     var u_gender;
     var u_avatar;
     u_gender = mw.cookie.get( 'user_gender' );
@@ -123,75 +119,5 @@
             }
         );
     }
-    WB2.anyWhere(function (W) {
-	    W.widget.connectButton({
-	        id: "wb_connect_btn",
-	        type: '3,2',
-	        callback: {
-	            login: function (o) { //登录后的回调函数
-	                // alert("login: " + o.screen_name)
-	                // console.log("o"+o);
-	                // var wbId = o.id; o.avatar_large gender: "m"
-	                if (o.gender == 'm'){
-			            gender = 'male';
-			        }else if(o.gender == 'f'){
-			            gender = 'female';
-			        }else{
-			            gender = 'unknown';
-			        }
-			        mw.cookie.set( 'user_gender', gender );
-			        mw.cookie.set( 'user_avatar', o.avatar_large );
-	                wbOpenId = o.id;
-	                $.post(
-		                mw.util.wikiScript(), {
-		                    action: 'ajax',
-		                    rs: 'wfCheckOauth',
-		                    rsargs: [wbOpenId, 'weibo']
-		                },
-		                function (data) {
-		                    var res = $.parseJSON(data);
-		                    if (res.success == true) {
-		                        if (res.result == null){
-		                        	var content = '';
-		                        	content = '<form><label for="wbloginname">用户名</label><input type="text" id="wbloginusername" class="form-control" value="'+o.name+'" name="wbloginname">'+
-		                            '<label for="wbloginemail">邮箱</label><input type="email" class="form-control" id="wbloginemail" placeholder="请输入邮箱" name="wbloginemail">'+
-		                        	'<label for="wbloginpass">密码</label><input type="password" id="wbloginpassword" class="form-control" placeholder="请输入密码" name="qqloginpass"><div class="mw-ui-button  mw-ui-block mw-ui-constructive" id="wbConfirm">提交</div></form>';
-		                            $('.qqlogin').modal({backdrop: 'static', keyboard: false});
-		                        	$('.qqlogin .modal-body').append(content);
-		                        }else{
-		                        	var userId = res.result;
-		                        	// alert(res.result);
-		                        	$.post(
-		                        		mw.util.wikiScript(), {
-					                    action: 'ajax',
-					                    rs: 'wfAddUserOauthCookie',
-					                    rsargs: [ userId ]
-					                },
-					                function (data) {
-					                	var res = $.parseJSON(data);
-		                    			if (res.success == true) {                   				                               
-				                            location.href = updateQueryStringParameter(location.href, 'loggingIn', '1');
-					                	}
-					                });
-					               
-		                        }
-		                    } 
-		                }
-		            );
-	            },
-	            logout: function () { //退出后的回调函数
-	                alert('logout');
-	            }
-	        }
-	    });
-	});
-	$("body").on("click","#wbConfirm",function(){
-		var username = $("#wbloginusername").val();
-		var email = $("#wbloginemail").val();
-		var pass = $("#wbloginpassword").val();
-		wiki_signup("weibo",username,email,pass,wbOpenId);
-	});
+
 });
-
-
-// WB2.checkLogin()   true or false
