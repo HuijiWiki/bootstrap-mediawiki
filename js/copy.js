4add8afee3bffe11624cfdc6355e3854b6c671e8
root <guxi99@gmail.com>
2015-12-30 15:00:46 +0800

    copy

--- js/copy.js
+++ js/copy.js
@@ -271,17 +271,15 @@ copyWiki.prototype ={
 };
 $(function() {
     jQuery.post(
-        mw.util.wikiScript(), {
-            action: 'ajax',
-            rs: 'wfGetForkInfoByPageId',
-            rsargs: [ mw.config.get('wgArticleId') ]
-        },
-        function( data ) {
-            var res = jQuery.parseJSON(data);
-            if (res.success){
-                console.log(res);
-            }
-        }
-    );
+    mw.util.wikiScript(), {
+        action: 'ajax',
+        rs: 'wfGetForkInfoByPageId',
+        rsargs: [mw.config.get('wgArticleId')]
+    },
+    function( data ) {
+        var content = '<span>来自<a href="'+data.fork_from+'.huiji.wiki"></span>';
+        $('#contentSub').append(content);
+    });
+    
     return new copyWiki();
 });
