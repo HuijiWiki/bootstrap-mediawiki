e15949034f4ccafc26d77cc6d7459f1f10cc2c85
root <guxi99@gmail.com>
2016-01-15 17:04:18 +0800

    edit category

--- js/editcategory.js
+++ js/editcategory.js
@@ -15,7 +15,7 @@ function addCategory(category){
         b[b.length-1] = b[b.length-1].substring(0,b[b.length-1].length-2);
         b.forEach(function(i){
             a += '<li><a href="/wiki/%E5%88%86%E7%B1%BB:'+i+'" title="分类:'+i+'">'+i+'</a></li>'
-        })
+        });
         $('#catlinks .last').before(a);
         $('#add-category-btn').popover('hide');
     } ).fail( function( code, result ) {
@@ -30,6 +30,8 @@ function addCategory(category){
 }
 function addSpan(){
     var val = $('.edit-field input').val();
+    var toggle = true;
+    val = val.replace(/\s+/g, ' ');
     var html = '<li class="edit-span"><i class="glyphicon glyphicon-remove"></i>'+val+'</li>';
     if(val == ''||val == ' '){
         mw.notification.notify('分类不能为空，请重新输入');
@@ -40,17 +42,28 @@ function addSpan(){
         if($(this).text()==val){
             mw.notification.notify('分类不能重复，请重新输入');
             $('.edit-field input').val('');
+            toggle = false;
             return;
         }
     });
-    $('.edit-field').before(html);
-    $('.edit-field input').val('');
+    if (toggle == true){
+        $('.edit-field').before(html);
+        $('.edit-field input').val('');
+    }
 }
 function getData(){
     var data='';
-    $('.edit-choices .edit-span').each(function(){
-        data += '[[Category:'+$(this).text()+']]';
-    });
+    var val = $('.edit-field input').val();
+    if($('.edit-choices .edit-span').length==0){
+        val = val.replace(/\s+/g, ' ');
+        if(val == ''|| val ==' ')
+        return;
+        data = '[[Category:' + val + ']]'
+    }else {
+        $('.edit-choices .edit-span').each(function () {
+            data += '[[Category:' + $(this).text() + ']]';
+        });
+    }
     return data;
 }
 $(function(){
@@ -83,6 +96,10 @@ $(function(){
     }).on('click','.edit-submit',function(e){
         e.stopPropagation();
         var data = getData();
+        if (data == undefined){
+            mw.notification.notify('内容不可为空');
+            return;
+        }
         addCategory(data);
     }).on('click','.edit-cancel',function(){
         $('#add-category-btn').popover('hide');
