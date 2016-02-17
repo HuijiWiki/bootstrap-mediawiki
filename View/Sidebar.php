aa4f268d48c061b5d9da0aa1d0a6d3f66783436d
root <guxi99@gmail.com>
2016-02-17 20:59:09 +0800

    add new buttons

--- View/Sidebar.php
+++ View/Sidebar.php
@@ -62,6 +62,7 @@
                 <?php if ( $wgEnableUploads ) { ?>
                     <li><a href="<?php echo $url_prefix; ?>Special:文件上传" class="upload-a-file" rel="nofollow"><i class="fa fa-upload"></i> 上传文件</a></li>
                 <?php } ?>
+                <li><a href="<?php echo $url_prefix; ?>帮助讨论:用户手册" class="upload-a-file" rel="nofollow"><i class="fa fa-question-circle"></i> 求助提问</a></li>
                 <?php if ( $wgHuijiPrefix !== 'www' && $this->data['isarticle'] ) { ?>
                 <li class="dropdown">
                     <a class="dropdown-toggle" data-toggle="dropdown" href="#">
