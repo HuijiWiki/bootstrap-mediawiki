4ab63b20efaef0430b5ecc0da1c376c19a3919a8
reasno <guxi99@gmail.com>
2016-03-28 14:56:12 +0800

    add login warning

--- js/huiji.ve.js
+++ js/huiji.ve.js
@@ -1,4 +1,4 @@
 mw.hook( 've.activationComplete' ).add( function() {
-	ga('send', 'event', 'VisualEditor', 'activate', 'aritcle', 1);
+	ga('send', 'event', 'VisualEditor', 'activate', 'article', 1);
 	// SOME CODE TO RUN WHEN EDITOR IS READY
 } );
