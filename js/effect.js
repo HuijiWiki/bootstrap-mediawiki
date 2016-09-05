381e9455715f88aa59db0b4b08655b04a69b4f8d
c86jeff <zhangjiexin0101@gmail.com>
2016-09-05 13:38:18 +0800

    fix icon bug

--- js/effect.js
+++ js/effect.js
@@ -307,9 +307,9 @@ $(document).ready(function(){
                                     mw.notification.notify('登录成功', options);
                                     // document.location.reload();
                                     if (mw.config.get('wgCanonicalSpecialPageName') === 'Userlogout'){
-                                        window.location.href = updateQueryStringParameter($('#mw-returnto a').attr('href'), 'loggingIn', '1');
+                                        window.location.href = window.updateQueryStringParameter($('#mw-returnto a').attr('href'), 'loggingIn', '1');
                                     }else {
-                                        window.locations.href = updateQueryStringParameter(location.href, 'loggingIn', '1');s
+                                        window.locations.href = window.updateQueryStringParameter(location.href, 'loggingIn', '1');s
                                     }
                                 }else{
                                     options = {
