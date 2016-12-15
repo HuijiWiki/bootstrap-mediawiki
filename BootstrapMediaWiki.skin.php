b3ad2956852e8e04e605cd9c63db1045f2a54d62
Reasno <guxi99@gmail.com>
2016-12-15 17:27:11 +0800

    rollback chagnes

--- BootstrapMediaWiki.skin.php
+++ BootstrapMediaWiki.skin.php
@@ -245,7 +245,7 @@ class BootstrapMediaWikiTemplate extends HuijiSkinTemplate {
                                         echo $ad->getBlockScreen();?>
                                 <header id="firstHeading" class="page-header">
                                     <div class="hidden-sm hidden-xs">
-                                    <?php $ad = new AdsManager($site);
+                                    <?php
                                         echo $ad->getWideHeader();
                                     ?>
                                     </div>
@@ -433,19 +433,15 @@ class BootstrapMediaWikiTemplate extends HuijiSkinTemplate {
                 bdMini: "2",
                 bdMiniList: false,
                 bdPic: "",
-                bdStyle: "2",
-                bdUrl:"http://<?php echo $wgHuijiPrefix.$wgCdnHuijiSuffix; ?>/index.php?curid=<?php echo $this->skin->getTitle()->getArticleId(); ?>",
+                bdStyle: false,
+                bdSign: "off",
+                bdUrl:"http://<?php echo $wgHuijiPrefix ?>.huiji.wiki/index.php?curid=<?php echo $this->skin->getTitle()->getArticleId(); ?>",
             },
             share: [
                 {
                     tag: "share_1",
                     bdSize: 16,
-                    bdCustomStyle:""
-                },
-                {
-                    tag: "share_2",
-                    bdSize: 16,
-                    bdCustomStyle:""
+                    bdCustomStyle:"http://cdn.huijiwiki.com/www/skins/bootstrap-mediawiki/css/huiji.bdshare.css"
                 }
             ]
         };with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];</script>        
